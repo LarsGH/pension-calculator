d1c0910aaa29fd2f882ed4b532fe374d4a895dd4
LarsGH <lars.syfuss@googlemail.com>
2022-04-15 15:19:01 +0200

    chore: handle unexpected calculation errors

--- src/main/java/de/lasy/pension/controller/PensionController.java
+++ src/main/java/de/lasy/pension/controller/PensionController.java
@@ -6,10 +6,12 @@ import de.lasy.pension.model.PensionCalculationResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 
@@ -32,7 +34,14 @@ public class PensionController {
     @PostMapping("/calculate")
     public PensionCalculationResult calculatePension(@Valid @RequestBody PensionCalculationInput calculationInput) {
         logger.info("pension/calculate requested with: {}", calculationInput);
-        // TODO: Validierung und Error-Handling! https://www.baeldung.com/exception-handling-for-rest-with-spring
-        return calculator.calculatePension(calculationInput);
+        try {
+            return calculator.calculatePension(calculationInput);
+        } catch (Exception e) {
+            // Unvorhergesehener Fehler bei der Berechnung
+            throw new ResponseStatusException(
+                    HttpStatus.UNPROCESSABLE_ENTITY,
+                    "Pension calculation failed for input.",
+                    e);
+        }
     }
 }
